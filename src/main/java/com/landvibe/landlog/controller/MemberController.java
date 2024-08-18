d0bb66c888d8b4278f5e0a400eb3977687b85a1b
Jihyun <Jihyun@gmail.com>
2024-08-19 08:26:07 +0900

    feat : 회원가입 필드 추가

--- src/main/java/com/landvibe/landlog/controller/MemberController.java
+++ src/main/java/com/landvibe/landlog/controller/MemberController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class MemberController {
@@ -20,12 +22,14 @@ public class MemberController {
     @GetMapping(value = "/members/new")
     public String createForm() {
         return "members/createMemberForm";
-    }
+    } //회원가입 폼 반환
 
     @PostMapping(value = "/members/new")
-    public String create(MemberForm form) {
+    public String create(MemberForm form) { //새로운 회원생성, 회원가입 후 메인 페이지로 리다이렉트
         Member member = new Member();
         member.setName(form.getName());
+        member.setEmail(form.getEmail());
+        member.setPwd(form.getPwd());
         memberService.join(member);
         return "redirect:/";
     }
@@ -36,4 +40,5 @@ public class MemberController {
         model.addAttribute("members", members);
         return "members/memberList";
     }
+
 }
--- src/main/java/com/landvibe/landlog/domain/Member.java
+++ src/main/java/com/landvibe/landlog/domain/Member.java
@@ -4,13 +4,17 @@ public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private String pwd;
 
     public Member() {
     }
 
-    public Member(Long id, String name) {
+    public Member(Long id, String name, String email, String pwd) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.pwd = pwd;
     }
 
     public Long getId() {
@@ -27,5 +31,21 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
     }
 }
