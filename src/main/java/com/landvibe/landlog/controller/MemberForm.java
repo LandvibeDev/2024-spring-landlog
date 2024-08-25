ebbc14ab1c7eb6fd81481e28f8cacc91734dfdd1
InSooBeen <isb8180@naver.com>
2024-08-25 12:42:28 +0900

    [Fix] 커맨드 객체 setter 추가

--- src/main/java/com/landvibe/landlog/controller/MemberForm.java
+++ src/main/java/com/landvibe/landlog/controller/MemberForm.java
@@ -13,6 +13,18 @@ public class MemberForm {
         return email;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
--- src/main/java/com/landvibe/landlog/controller/MemberLoginForm.java
+++ src/main/java/com/landvibe/landlog/controller/MemberLoginForm.java
@@ -8,6 +8,14 @@ public class MemberLoginForm {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
